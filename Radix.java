ee6010cae1909ec2e3e89b887c5aadc8a28a1a39
Cameron Nelson <cjnelson2004@gmail.com>
2021-01-11 20:58:30 -0500

    Initial commit

---
+++ Radix.java
@@ -0,0 +1,14 @@
+public class Radix {
+	public static int nth(int n, int col) {
+		return (int)(n % Math.pow(10,col+1)) / (int)(Math.pow(10,col));
+	}
+	public static int length(int n) {
+		return (int)(Math.log10(n));
+	}
+	public static void merge(SortableLinkedList original, SortableLinkedList[] buckets) {
+		for (int i = 0; i < buckets.length; i++) {
+			original.extend(buckets[i]);
+		}
+	}
+	public static void
+}
